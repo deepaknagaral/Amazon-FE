833e72be6843a13c18b5199130f1a05ee2e17357
deepaknagaral <158196315+deepaknagaral@users.noreply.github.com>
2024-05-04 00:13:35 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ravi-project-s3bucket"  # Replace with your actual S3 bucket name
+    bucket = "daamodar-s3-bucket"  # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"  
-    region = "ap-south-1" 
+    region = "ap-northeast-1" 
   }
 }
